8ba3ca8d106165fb21c8bc257039df42feae78ad
Sean Packham <sean@seanpackham.com>
2018-01-26 21:57:22 +0000

    Company and name generation

---
+++ profiles/main.lua
@@ -0,0 +1,16 @@
+
+local profiles = require('profiles')
+
+function love.load()
+
+end
+
+function love.update(dt)
+  if love.keyboard.isDown('escape') then
+    love.event.quit()
+  end
+end
+
+function love.draw()
+
+end
