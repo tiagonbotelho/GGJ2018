8f319ef2388fa46e502598152f2b6b6511fc7db7
Tiago Botelho <tiagonbotelho@hotmail.com>
2018-01-26 20:34:10 +0000

    Initial commit

---
+++ main.lua
@@ -0,0 +1,31 @@
+local vector = require "vector"
+
+local window = {}
+local sidebar = {}
+sidebar.position = vector(0,0)
+sidebar.thickness = 10
+sidebar.height = love.graphics.getHeight()
+local main_window = {}
+
+function window.construct_window()
+
+end
+
+function love.load()
+
+end
+
+function love.update(dt)
+end
+
+function love.draw()
+
+end
+
+function love.keyreleased(key, code)
+end
+
+function love.quit()
+  print("Thanks for playing! Come back soon!")
+end
+
