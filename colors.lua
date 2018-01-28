31ad4371a22b6722d01afb0398e5212b6da91afd
Tiago Botelho <tiagonbotelho@hotmail.com>
2018-01-28 11:53:59 +0000

    removes all lua code

--- colors.lua
+++
@@ -1,9 +0,0 @@
-local colors = {}
-colors.white = { red = 255, green = 255, blue = 255 }
-colors.black = { red = 0, green = 0, blue = 0 }
-colors.gray = { red = 113, green = 114, blue = 116 }
-colors.purple = { red = 77, green = 57, blue = 75 }
-colors.light_purple = { red = 171, green = 155, blue = 169 }
-colors.green = { red = 76, green = 150, blue = 137 }
-
-return colors
--- fonts.lua
+++
@@ -1,8 +0,0 @@
-local fonts = {}
-fonts.lato_black = love.graphics.newFont("fonts/Lato-Black.ttf", 18)
-fonts.lato_bold = love.graphics.newFont("fonts/Lato-Bold.ttf", 16)
-fonts.lato_regular_increased = love.graphics.newFont("fonts/Lato-Regular.ttf", 18)
-fonts.lato_regular = love.graphics.newFont("fonts/Lato-Regular.ttf", 14)
-
-
-return fonts
